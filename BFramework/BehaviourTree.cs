c2f80ced6ff8f02235a1e8309cfdaedf114fa3dd
Blake.He <blake067@foxmail.com>
2018-01-26 13:19:21 +0800

    Update

--- BFramework/BehaviourTree.cs
+++ BFramework/BehaviourTree.cs
@@ -8,5 +8,22 @@ namespace BFramework
 {
     class BehaviourTree
     {
+        public class Node
+        {
+            Node(String name)
+            {
+                _name = name;
+            }
+
+            String _name = "default";
+            enum State
+            {
+                Stop = 0,
+                Working = 1,
+                Pause = 2,
+            }
+           
+            public string Name { get => _name; private set => _name = value; }
+        }
     }
 }
--- BFramework/ObjectPool.cs
+++ BFramework/ObjectPool.cs
@@ -1,13 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections;
 
 namespace BFramework
 {
     public class ObjectPool
     {
+        private interface IShellObject
+        {
+            void Create(Object param);
+            Object GetInnerObject();
+            bool IsValidate();
+            void Release();
+        }
+
+        private class ShellObject
+        {
+            private IShellObject _shell;
+            private bool _beUsing;
+            private Type _type;
+            private Object _createParam;
+
+            public ShellObject(Type type, Object param)
+            {
+                _type = type;
+                _createParam = param;
+                Create();
+            }
+
+            private void Create()
+            {
+                _beUsing = false;
+                _shell = (IShellObject)Activator.CreateInstance(_type);
+                _shell.Create(_createParam);
+            }
+        }
+
         private Dictionary<Object, Queue<Object>> _pool = new Dictionary<object, Queue<object>>();
-        private Dictionary<Object, Object> _tag = new Dictionary<object, object>();
+        private Dictionary<Object, Type> _types = new Dictionary<object, Type>();
+        private Dictionary<Object, int> _caches = new Dictionary<object, int>();
+        private Dictionary<Object, Object> _tags = new Dictionary<object, object>();
         private Object _pointer;
         
         public List<Object> GetKeys()
@@ -30,14 +61,21 @@ namespace BFramework
         public void Clear()
         {
             _pool.Clear();
-            _tag.Clear();
+            _tags.Clear();
+        }
+
+        public void CreateNewQueue(Object key, Type itemType, int cache)
+        {
+            _pool.Add(key, new Queue<object>());
+            _caches.Add(key, cache > 1 ? cache : 1);
+            _types.Add(key, itemType);
         }
 
         public void Add(Object key, Object item)
         {
             if (!_pool.ContainsKey(key))
             {
-                _pool.Add(key, new Queue<object>());
+                CreateNewQueue(key, item.GetType(), 2);
             }
             _pool[key].Enqueue(item);
         }
@@ -66,20 +104,26 @@ namespace BFramework
             {
                 return;
             }
-            RemoveMark(item);
+            if (_tags.ContainsKey(item))
+            {
+                _pool[_tags[item]].Enqueue(item);
+                _tags.Remove(item);
+            }
         }
 
         private void MarkAsOut(Object item, Object key)
         {
-            _tag.Add(item, key);
+            _tags.Add(item, key);
         }
 
-        private void RemoveMark(Object item)
+        private void FillPool(Object key)
         {
-            if (_tag.ContainsKey(item))
+            if(_pool[key].Count < _caches[key])
             {
-                _pool[_tag[item]].Enqueue(item);
-                _tag.Remove(item);
+                for (int i = _caches[key] - _pool[key].Count; i >= 0; i--)
+                {
+                    _pool[key].Enqueue(Activator.CreateInstance(_types[key]));
+                }
             }
         }
     }
