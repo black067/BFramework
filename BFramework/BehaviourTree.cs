bf1a1c9f9ffdd3bf21fadf3e4f2257dc20236339
Blake.He <blake067@foxmail.com>
2018-01-27 13:36:14 +0800

    Update

--- BFramework/BehaviourTree.cs
+++ BFramework/BehaviourTree.cs
@@ -8,7 +8,7 @@ namespace BFramework
 {
     class BehaviourTree
     {
-        public enum Status
+        public enum STATUS
         {
             SUSPENDED = 0,
             RUNNING = 1,
@@ -18,13 +18,55 @@ namespace BFramework
         public interface IBehaviour
         {
             void OnInitialize();
-            Status Update();
-            void OnTerminate(Status status);
+            STATUS Update();
+            STATUS OnTerminate(STATUS status);
         }
-        
-        public void Update()
-        {
 
+        public class Behaviour : IBehaviour
+        {
+            private STATUS _status;
+
+            public virtual void OnInitialize() { }
+
+            public virtual STATUS OnTerminate(STATUS status)
+            {
+                return STATUS.SUCCESS;
+            }
+
+            public virtual STATUS Update()
+            {
+                return STATUS.SUCCESS;
+            }
+
+            public STATUS Tick()
+            {
+                if (_status != STATUS.RUNNING)
+                {
+                    OnInitialize();
+                }
+                _status = Update();
+                if (_status != STATUS.RUNNING)
+                {
+                    _status = OnTerminate(_status);
+                }
+                return _status;
+            }
+        }
+
+        public class Action : Behaviour { }
+
+        public class Decorator : Behaviour
+        {
+            Behaviour _child;
+            public Decorator(ref Behaviour behaviour)
+            {
+                _child = behaviour;
+            }
+        }
+
+        public void Tick()
+        {
+            Behaviour A = new Behaviour();
         }
     }
 }
